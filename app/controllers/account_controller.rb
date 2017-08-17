44efa72753e7eeccee5e3ab6bd955e1687b75b3d
Kevin Geng <gengx060@gmail.com>
2017-08-17 15:02:24 -0400

    start to support create account and password, and universal login

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -1,5 +1,27 @@
 class AccountController < ApplicationController
+	before_action :check_auth, :except => ["login", "create_password_from_url_hash"]
+
 	def new_user
+		if session[:is_org]
+			render :json => {:error => 'You don\'t have such permission.'}, :status => 500
+		elsif User.exists?(email: params['email'])
+			render :json => {:error => 'Duplicate email found.'}, :status => 500
+		else
+			user = User.new do |u|
+				u.firstname = params['firstname']
+				u.lastname = params['lastname']
+				u.email = params['email']
+				# salt, hashed = create_password_salt('gege1818')
+				# u.salt = salt
+				# u.password = hashed
+				u.org_id = session[:user_id]
+			end
+			user.save!
+			render :json => {:success => true}
+		end
+	end
+
+	def create_FB_account
 		if session[:is_org]
 			render :json => {:error => 'You don\'t have such permission.'}, :status => 500
 		elsif User.exists?(email: params['email'])
@@ -38,22 +60,34 @@ class AccountController < ApplicationController
 		end
 	end
 
-	def create_password_from_url_hash
-		unless params['user_id'] && params['password']
-			render :json => {:message => 'Missing password or user id.'}, :status => 500
+	def verify_from_url_hash
+		unless params['hash']
+			render :json => {:message => 'Missing hash token.'}, :status => 500
 			return
 		end
-		if session[:is_org]
-			user = User.find_by(:id => params['user_id'], :org_id => session[:user_id])
+		uh = UserHash.find_by(:hash => params['hash'], :claimed => null)
+		if uh
+			render :json => {:message => 'Hash verified successfully.'}
+		else
+			render :json => {:message => 'Hash does not exist.'}, status => 500
+		end
+	end
+
+	def create_password_from_url_hash
+		unless params['hash']
+			render :json => {:message => 'Missing hash token.'}, :status => 500
+			return
+		end
+		uh = UserHash.find_by(:hash => params['hash'], :claimed => null)
+		if uh
+			user = User.find(uh.user_id)
 			if user
 				user.salt, user.password = create_password_salt(params['password'])
 				user.save!
 				render :json => {:message => 'Password updated successfully.'}
 			else
-				render :json => {:message => 'User is not part of your organizaiton.'}, status => 500
+				render :json => {:message => 'Hash or user does not exist.'}, status => 500
 			end
-		else
-			render :json => {:message => 'You don\'t have such permission.'}, status => 500
 		end
 	end
 
