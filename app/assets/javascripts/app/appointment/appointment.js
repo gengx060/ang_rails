ff681a418941383fe5f69a99bd975382af6a78fd
Kevin Geng <gengx060@gmail.com>
2017-08-25 14:58:20 -0400

    working on creating event

--- app/assets/javascripts/app/appointment/appointment.js
+++ app/assets/javascripts/app/appointment/appointment.js
@@ -1,186 +1,202 @@
-define(['jquery', 'angular', 'Enumerable', 'moment', 'fullcalendar',
+define(['angular', 'Enumerable', 'moment', 'fullcalendar',
 		'angular-modal-service'],
-	function ($, angular, Enumerable, moment, fullcalendar) {
+	function (angular, Enumerable, moment, fullcalendar) {
 		angular.module('appointment', ['angularModalService'])
-			.directive('appointment', function () {
-				return {
-					require: '^tabs',
-					restrict: 'E',
-					transclude: true,
-					scope: {},
-					controller: function ($scope, $element, $location, $routeParams, ModalService) {
-						$scope.view_types = ['month', 'agendaWeek', 'agendaDay'];
-						$scope.view_type = 'month';
-						fullcalendar;
-						$scope.fullCalendar;
+		.directive('appointment', function () {
+			return {
+				require: '^tabs',
+				restrict: 'E',
+				transclude: true,
+				scope: {},
+				controller: function ($scope, $element, $location, $routeParams, ModalService) {
+					$scope.view_types = ['month', 'agendaWeek', 'agendaDay'];
+					$scope.datetimes = [
+						'12:00 am', '12:30 am', '1:00 am', '1:30 am', '2:00 am', '2:30 am',
+						'3:00 am', '3:30 am', '4:00 am', '4:30 am', '5:00 am', '5:30 am',
+						'6:00 am', '6:30 am', '7:00 am', '7:30 am', '8:00 am', '8:30 am',
+						'9:00 am', '9:30 am', '10:00 am', '10:30 am', '11:00 am', '11:30 am',
+						'12:00 pm', '12:30 pm', '1:00 pm', '1:30 pm', '2:00 pm', '2:30 pm',
+						'3:00 pm', '3:30 pm', '4:00 pm', '4:30 pm', '5:00 pm', '5:30 pm',
+						'6:00 pm', '6:30 pm', '7:00 pm', '7:30 pm', '8:00 pm', '8:30 pm',
+						'9:00 pm', '9:30 pm', '10:00 pm', '10:30 pm', '11:00 pm', '11:30 pm']
+					$scope.view_type = 'month';
+					$scope.fullCalendar;
 
-						$scope.fullCalendar_option = {
-							header: {
-								left: 'prev,next today',
-								center: 'title',
-								right: 'month,agendaWeek,agendaDay'
+					$scope.fullCalendar_option = {
+						header: {
+							left: 'prev,next today',
+							center: 'title',
+							right: 'month,agendaWeek,agendaDay'
+						},
+						minTime: "00:00:00", // "06:00:00",
+						maxTime: "24:00:00", // "18:00:00",
+						defaultDate: moment(),
+						defaultView: 'month',
+						editable: true,
+						fixedWeekCount: false,
+						slotDuration: '00:10:00', // 10 minutes for each row
+						slotLabelFormat: 'h(:mm)a',
+						dayClick: function (date, jsEvent, view) {
+							// console.log(this.events);
+							prevTime = typeof currentTime === 'undefined' || currentTime === null
+								? new Date().getTime() - 1000000
+								: currentTime;
+							currentTime = new Date().getTime();
+
+							if (currentTime - prevTime < 500) {
+								if ($scope.fullCalendar.fullCalendar('getView').type == 'month') {
+									$scope.$apply(function () {
+										$location.search('newevent', date.format("YYYY-MM-DD HH:mm"));
+										// $location.search('view', 'agendaWeek');
+										// $location.search('date', date.format());
+									});
+								} else if (['agendaWeek', 'agendaDay'].indexOf($routeParams.view) > -1) {
+									$scope.$apply(function () {
+										$location.search('newevent', date.format("YYYY-MM-DD HH:mm"));
+									});
+								}
+								//double click call back
+							}
+						},
+						eventClick: function (event, jsEvent, view) {
+							//set the values and open the modal
+							debugger
+							// $location.search('newevent', date.format("YYYY-MM-DD HH:mm"));
+							// $("#eventInfo").html(event.description);
+							// $("#eventLink").attr('href', event.url);
+						},
+						events: [
+							{
+								id: 998,
+								name: 'Repeating Event',
+								start: '2014-06-16 16:00'
 							},
-							minTime: "06:00:00",
-							maxTime: "18:00:00",
-							defaultDate: moment(),
-							defaultView: 'month',
-							editable: true,
-							fixedWeekCount: false,
-							slotDuration: '00:10:00', // 10 minutes for each row
-							dayClick: function (date, jsEvent, view) {
-								// console.log(this.events);
-								prevTime = typeof currentTime === 'undefined' || currentTime === null
-									? new Date().getTime() - 1000000
-									: currentTime;
-								currentTime = new Date().getTime();
+							{
+								id: 999,
+								name: 'Meeting',
+								start: '2017-08-24 10:30',
+								end: '2017-08-24 12:30',
+								dkslkfl: 'adfaf'
+							}
+						]
+					};
 
-								if (currentTime - prevTime < 500) {
-									if ($scope.fullCalendar.fullCalendar('getView').type == 'month') {
+					$scope.show_update_hash = function () {
+						$location.search('newuser', null);
+						$location.search('view', 'month');
+					};
+
+					$scope.query_params = function () {
+						if ($scope.view_types.indexOf($routeParams.view) == -1) {
+							$location.search('view', 'month');
+							$location.search('date', moment().format("YYYY-MM-DD"));
+							return;
+						}
+						else {
+							if (!$scope.fullCalendar) {
+								$scope.fullCalendar_option.defaultView = $routeParams.view;
+								$scope.fullCalendar_option.defaultDate = $routeParams.date;
+								$(document).ready(function () {
+									$scope.fullCalendar = $('#fullcalendar_div_a').fullCalendar($scope.fullCalendar_option)
+									$('.fc-agendaMonth-button').unbind("click");
+									$('.fc-agendaWeek-button').unbind("click");
+									$('.fc-agendaDay-button').unbind("click");
+									$('.fc-today-button').unbind("click");
+									$('.fc-icon-left-single-arrow').unbind("click");
+									$('.fc-icon-right-single-arrow').unbind("click");
+
+
+									$scope.fullCalendar
+									.on('click', '.fc-month-button', function () {
+										$scope.$apply(function () {
+											$location.search('view', 'month');
+										});
+									})
+									.on('click', '.fc-agendaWeek-button', function () {
 										$scope.$apply(function () {
 											$location.search('view', 'agendaWeek');
-											$location.search('date', date.format());
 										});
-									} else if (['agendaWeek', 'agendaDay'].indexOf($routeParams.view) > -1) {
+									})
+									.on('click', '.fc-agendaDay-button', function () {
 										$scope.$apply(function () {
-											$location.search('newevent', date.format("YYYY-MM-DD HH:mm"));
+											$location.search('view', 'agendaDay');
 										});
-									}
-									//double click call back
-								}
-							},
-							eventClick: function (event, jsEvent, view) {
-								//set the values and open the modal
-								debugger
-								// $location.search('newevent', date.format("YYYY-MM-DD HH:mm"));
-								// $("#eventInfo").html(event.description);
-								// $("#eventLink").attr('href', event.url);
-							},
-							events: [
-								{
-									id: 998,
-									name: 'Repeating Event',
-									start: '2014-06-16 16:00'
-								},
-								{
-									id: 999,
-									name: 'Meeting',
-									start: '2017-08-24 10:30',
-									end: '2017-08-24 12:30',
-									dkslkfl: 'adfaf'
-								}
-							]
-						};
-
-						$scope.show_update_hash = function () {
-							$location.search('newuser', null);
-							$location.search('view', 'month');
-						};
-
-						$scope.query_params = function () {
-							if ($scope.view_types.indexOf($routeParams.view) == -1) {
-								$location.search('view', 'month');
-								$location.search('date', moment().format("YYYY-MM-DD"));
-								return;
-							}
-							else {
-								if (!$scope.fullCalendar) {
-									$scope.fullCalendar_option.defaultView = $routeParams.view;
-									$scope.fullCalendar_option.defaultDate = $routeParams.date;
-									$(document).ready(function () {
-										$scope.fullCalendar = $('#fullcalendar_div_a').fullCalendar($scope.fullCalendar_option)
-										$('.fc-agendaMonth-button').unbind("click");
-										$('.fc-agendaWeek-button').unbind("click");
-										$('.fc-agendaDay-button').unbind("click");
-										$('.fc-today-button').unbind("click");
-										$('.fc-icon-left-single-arrow').unbind("click");
-										$('.fc-icon-right-single-arrow').unbind("click");
-
-
-										$scope.fullCalendar
-											.on('click', '.fc-month-button', function () {
-												$scope.$apply(function () {
-													$location.search('view', 'month');
-												});
-											})
-											.on('click', '.fc-agendaWeek-button', function () {
-												$scope.$apply(function () {
-													$location.search('view', 'agendaWeek');
-												});
-											})
-											.on('click', '.fc-agendaDay-button', function () {
-												$scope.$apply(function () {
-													$location.search('view', 'agendaDay');
-												});
-											})
-											.on('click', '.fc-icon-left-single-arrow', function () {
-												$scope.$apply(function () {
-													$location.search('date',
-														($scope.fullCalendar.fullCalendar('getDate') - 1).format("YYYY-MM-DD"));
-												});
-											})
-											.on('click', '.fc-icon-right-single-arrow', function () {
-												$scope.$apply(function () {
-													$location.search('date',
-														$scope.fullCalendar.fullCalendar('getDate').add(1, 'days').format());
-												});
-											})
-											.on('click', '.fc-today-button', function () {
-												$scope.$apply(function () {
-													$location.search('date', moment().format("YYYY-MM-DD"));
-												});
-											});
-									});
-								}
-								else {
-									if ($scope.fullCalendar.fullCalendar('getView').type != $routeParams.view) {
-										$scope.fullCalendar.fullCalendar('changeView', $routeParams.view, $routeParams.date);
-									}
-									if ($scope.fullCalendar.fullCalendar('getDate').format() != $routeParams.date) {
-										var date = $routeParams.date;
-										if (!moment($routeParams.date, 'YYYY-MM-DD', true).isValid()) {
-											date = moment().format('YYYY-MM-DD')
-										}
-										$scope.fullCalendar.fullCalendar('gotoDate', date);
-									}
-								}
-							}
-							if (moment($routeParams.newevent, 'YYYY-MM-DD HH:mm', true).isValid()) {
-								$scope.event = {
-									start: $routeParams.newevent,
-									end: $routeParams.newevent,
-									flag: ''
-								};
-								ModalService.showModal({
-									templateUrl: 'assets/app/appointment/new-event-modal.template.html',
-									controller: "ModalController",
-									scope: $scope
-								}).then(function (modal) {
-									modal.element.modal();
-									modal.close.then(function (result) {
-										$location.search('newevent', null);
+									})
+									.on('click', '.fc-icon-left-single-arrow', function () {
+										$scope.$apply(function () {
+											$location.search('date',
+												($scope.fullCalendar.fullCalendar('getDate') - 1).format("YYYY-MM-DD"));
+										});
+									})
+									.on('click', '.fc-icon-right-single-arrow', function () {
+										$scope.$apply(function () {
+											$location.search('date',
+												$scope.fullCalendar.fullCalendar('getDate').add(1, 'days').format());
+										});
+									})
+									.on('click', '.fc-today-button', function () {
+										$scope.$apply(function () {
+											$location.search('date', moment().format("YYYY-MM-DD"));
+										});
 									});
 								});
-							} else if ($routeParams.newevent) {
-								$location.search('newevent', null);
 							}
-						};
-						$scope.$on('$routeUpdate', function () {
-							$scope.query_params();
-						});
-
+							else {
+								if ($scope.fullCalendar.fullCalendar('getView').type != $routeParams.view) {
+									$scope.fullCalendar.fullCalendar('changeView', $routeParams.view, $routeParams.date);
+								}
+								if ($scope.fullCalendar.fullCalendar('getDate').format() != $routeParams.date) {
+									var date = $routeParams.date;
+									if (!moment($routeParams.date, 'YYYY-MM-DD', true).isValid()) {
+										date = moment().format('YYYY-MM-DD')
+									}
+									$scope.fullCalendar.fullCalendar('gotoDate', date);
+								}
+							}
+						}
+						if (moment($routeParams.newevent, 'YYYY-MM-DD HH:mm', true).isValid()) {
+							var date = moment($routeParams.newevent).format('YYYY-MM-DD');
+							var time = moment($routeParams.newevent).format('h:mm a');
+							time = (time == "12:00 am" ? "8:00 am" : time);
+							$scope.event = {
+								start: date,
+								end: date,
+								start_time: time,
+								end_time: moment(time, 'h:mm a').add(30, 'm').format('h:mm a'),
+								title: ''
+							};
+							console.log($scope.event);
+							ModalService.showModal({
+								templateUrl: 'assets/app/appointment/new-event-modal.template.html',
+								controller: "ModalController",
+								scope: $scope
+							}).then(function (modal) {
+								modal.element.modal();
+								modal.close.then(function (result) {
+									$location.search('newevent', null);
+								});
+							});
+						} else if ($routeParams.newevent) {
+							$location.search('newevent', null);
+						}
+					};
+					$scope.$on('$routeUpdate', function () {
 						$scope.query_params();
-					},
-					templateUrl: 'assets/app/appointment/appointment.template.html',
-					replace: true
-				};
-			})
-			.controller('ModalController', function ($scope, $location, close) {
-				$scope.close = function (result) {
-					close(result, 500); // close, but give 500ms for bootstrap to animate
-				}
-				$scope.submit = function (result) {
-					console.log(500); // close, but give 500ms for bootstrap to animate
-				};
-			});
+					});
+
+					$scope.query_params();
+				},
+				templateUrl: 'assets/app/appointment/appointment.template.html',
+				replace: true
+			};
+		})
+		.controller('ModalController', function ($scope, $location, close) {
+			$scope.close = function (result) {
+				close(result, 500); // close, but give 500ms for bootstrap to animate
+			}
+			$scope.submit = function (result) {
+				console.log(500); // close, but give 500ms for bootstrap to animate
+			};
+		});
 	})
 ;
