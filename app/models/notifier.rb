47d7d3c8f75960e176b13407cbd84e182d1e3bb3
Kevin Geng <gengx060@gmail.com>
2017-08-31 20:54:35 -0400

    actionmailer is working

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -1,25 +1,27 @@
 class Notifier < ActionMailer::Base
 
-	default sender: "Mail Services <gengx064@gmail.com>"
+	default sender: "Mail Services <qshhz1@gmail.com>"
 	# EXACTOR_ACCOUNT_SERVICES_EMAIL = "Mail Services <gengx064@gmail.com>"
 
 
 	def signup_application_received(params)
 		# Email header info MUST be added here
 		recipients params[:email_to]
-		from params[:email_from]
-		subject params[:email_subject]
+		subject params[:emailSubject]
 		content_type "text/html"
 
 		# Email body substitutions go here
 		body params
 	end
 
-	def welcome(recipient)
-		# attachments['free_book.pdf'] = File.read('path/to/file.pdf')
-		mail(to:  recipient,
-			 bcc: ["gengx064@gmail.com"],
-			subject: "New account information")
+	def welcome(params)
+		@username = params[:username]
+		mail(to: nil, #params[:email],
+				 bcc: ["gengx064@gmail.com"],
+				 content_type: "text/html",
+				 subject: params[:emailSubject]) do |format|
+			format.html
+		end
 	end
 
 end
--- config/environment.rb
+++ config/environment.rb
@@ -11,14 +11,14 @@ ActionMailer::Base.raise_delivery_errors = true
 ActionMailer::Base.smtp_settings = {
 		address: "smtp.gmail.com",
 		port: 587,
-		user_name: "gengx064@gmail.com",
-		password: "gege@841028",
+		user_name: "qshhz1@gmail.com",
+		password: "gege841028",
 		authentication: :login,
 		domain: 'smtp.gmail.com',
 		tsl:                  true,
 		# enable_starttls_auto: true,
 		# enable_starttls_auto: false,
-		outgoing_address: "gengx064@gmail.com",
+		outgoing_address: "Mail Services <qshhz1@gmail.com>",
 		openssl_verify_mode: 'none'
 }
 
