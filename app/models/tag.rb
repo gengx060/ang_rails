67ac0a6bc9fda47e84504c952eb6be32db579de4
Kevin Geng <gengx060@gmail.com>
2017-09-25 14:58:22 -0400

    start working on tags

---
+++ app/models/tag.rb
@@ -0,0 +1,10 @@
+class Tag < ActiveRecord::Base
+
+	def self.read(params)
+		en = self.find(params[:id])
+		if en
+			en.is_read = "\1"
+			en.save!
+		end
+	end
+end
---
+++ app/models/user_tag.rb
@@ -0,0 +1,10 @@
+class UserTag < ActiveRecord::Base
+
+	def self.read(params)
+		en = self.find(params[:id])
+		if en
+			en.is_read = "\1"
+			en.save!
+		end
+	end
+end
